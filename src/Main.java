5ad3d961e6a0d98bf3b23ff69f2d1e61c29cfb3e
Дмитрий <prostodimas1337@gmail.com>
2024-11-08 17:46:55 +0300

    Program 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int listSize;
+        while (true) {
+            if(scanner.hasNextInt()) {
+                listSize = scanner.nextInt();
+                break;
+            } else{
+                System.out.println("Александр Владимирович, введите, пожалуйста, число");
+                scanner.next();
+            }
         }
+        ArrayList<Integer> priceDinner = new ArrayList<>();    //список для цен на обеды
+        ArrayList<Integer> dinnerTicket = new ArrayList<>();   //список для индексов с ценами >100
+        int tickets = 0;
+        int minPrice;
+        int sumPrice = 0;
+        for(int i = 0; i<listSize; i++){
+            while (true) {
+                if(scanner.hasNextInt()) {
+                    priceDinner.add(scanner.nextInt());
+                    sumPrice += priceDinner.get(i);         //считаем сумму всех обедов
+                    if(priceDinner.get(i) > 100){         //если ввели больше 100, прибавляем купон, и запоминаем индекс
+                        tickets++;
+                        dinnerTicket.add(i);
+                    }
+                    break;
+                } else{
+                    System.out.println("Александр Владимирович, введите, пожалуйста, число");
+                    scanner.next();
+                }
+            }
+        }
+        minPrice = sumPrice;
+        System.out.println(getMinPrice(priceDinner, dinnerTicket, sumPrice, tickets, minPrice));
+    }
+
+    private static int getMinPrice(ArrayList<Integer> priceDinner, ArrayList<Integer> dinnerTicket, int sumPrice, int tickets, int minPrice) {
+        if(dinnerTicket.isEmpty() || tickets == 0){               //если индексы или купоны закончились, возвращаем минимум
+            return minPrice;
+        }
+        ArrayList<Integer> dinnerToMin = new ArrayList<>();                 //список для цен после индекса цены >100
+        for(int i = dinnerTicket.getLast() + 1; i < priceDinner.size(); i++ ){           //с индекса цены >100 добавляем цены
+            dinnerToMin.add(priceDinner.get(i));
+        }
+        dinnerToMin.sort(Collections.reverseOrder());                //сортируем в обратном порядке
+        int dinnerToDelete = 0;
+        for(int i = 0; i < tickets && i < dinnerToMin.size(); i++){           //считаем сумму максимальных цен после цены >100 (не включительно) в кол-ве купонов/размерности списка(если билетов больше, чем цен)
+            dinnerToDelete += dinnerToMin.get(i);
+        }
+        int newMinPrice = sumPrice - dinnerToDelete;                 //"удаляем" максимальную сумму для удаления из общей цены
+        if(newMinPrice < minPrice){                               //если получилось меньше, меняем минимум
+            minPrice = newMinPrice;
+        }
+        dinnerTicket.removeLast();                     //удаляем индекс последней цены >100, чтобы работать со след. подмассивом
+        return getMinPrice(priceDinner, dinnerTicket, sumPrice, tickets-1, minPrice);   //уменьшем кол-во купонов на 1, т.к. теперь мы рассматриваем
+                                                                                               //подмассив начиная с более ранней цены >100
     }
 }
